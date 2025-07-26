826b71bf191b43451ef222434a38f42b62677ea6
Beeve <ckc40c@gmail.com>
2025-07-26 14:31:44 -0700

    got prime, happy, and perfect working
    also laid out a number of other functions I will write later
    
    I'm thinking about whether I even want to touch other bases or not

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,47 @@
 import { useState } from 'react'
-import { isPrime, isHappy } from './Math'
+import {isPrime, isHappy, isTaxicab, isPerfect} from './Math'
 import './App.css'
 
 function App() {
-  
+
+    const [numberToCheck, setNumberToCheck] = useState(0)
+    const [isPrimeNumber, setIsPrimeNumber] = useState(false)
+    const [isHappyNumber, setIsHappyNumber] = useState(false)
+    const [isTaxicabNumber, setIsTaxicabNumber] = useState(false)
+    const [isPerfectNumber, setIsPerfectNumber] = useState(false)
+
+    function checkNumberProperties() {
+        const number = parseInt(numberToCheck, 10)
+        setIsPrimeNumber(isPrime(number))
+        setIsHappyNumber(isHappy(number))
+        setIsTaxicabNumber(false) // Placeholder for future implementation
+        setIsPerfectNumber(isPerfect(number)) 
+    }
+    
+
+  return (
+    <>
+    <div> <h1>Number Fun Properties</h1>
+         </div>
+    <div className = "input">
+        <label>
+            
+            <button onClick = {() => { 
+                checkNumberProperties()
+            }}>What type of number is this? </button>
+            <input type="number" value={numberToCheck} onChange={(e) => setNumberToCheck(e.target.value)} />
+        </label>
+        
+    </div>
+    <div className = "output">
+        <h2>Results:</h2>
+        <p>{numberToCheck} is {isPrimeNumber ? '' : 'not '}a prime number.</p>
+        <p>{numberToCheck} is {isHappyNumber ? '' : 'not '}a happy number.</p>
+        <p>{numberToCheck} is {isPerfectNumber ? '' : 'not '} a perfect number. </p>
+    </div>
+    </>
+  )
 }
+
 
 export default App
--- src/Math.jsx
+++ src/Math.jsx
@@ -8,12 +8,64 @@ function isPrime(num) {
 
 function isHappy(num) {
     let sum =0
+    let isHappy = false;
     num = num.toString();
-    for (let i = 0; i < 100; i++){
+    for (let i = 0; i < 100; i++) {
+      sum = 0;
         for (let j = 0; j < num.length; j++) {
-            
-            sum += (num.chartAt(j)  * num.charAt(j));
+            let digit = parseInt(num.charAt(j), 10);
+            if (isNaN(digit)) return false; // Handle non-numeric input
+            sum += (digit * digit);
         }
-        if (sum === 1) return true;
+        if (sum === 1) return isHappy = true; 
+
+        else{
+          num = sum.toString();
+          
+        }
     }
 }
+
+function isTaxicab(num) {
+    return null;
+}
+
+function isPerfect(num) {
+  let divisorSum = 0
+  for (let i =0; i<num; i++){
+    if (num % i  === 0 ) {
+      divisorSum += i;
+    }
+  }
+
+  if ( divisorSum === num) {
+    return true;
+  }
+} 
+
+
+
+function isAbundant(num) {
+  let divisorSum = 0;
+  for (let i = 1; i < num; i++) {
+    if (num % i === 0) {
+      divisorSum += i;
+    }
+  }
+  return divisorSum > num;
+} 
+
+function isSemiPerfect(num) {
+  return false; // Placeholder for future implementation
+}
+
+function isWeird(num) {
+  if (isAbundant(num) && !isSemiPerfect(num)) {
+    return true;
+  }
+  return false;
+}
+
+
+
+export { isPrime, isHappy, isTaxicab, isPerfect };
